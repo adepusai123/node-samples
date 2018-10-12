30a4c3ad9bbb637375c600bfbc73b9fade273712
Sai Kumar <saikumara@wavelabs.in>
2018-10-12 14:43:29 +0530

    Google and Twitter Auth code

--- node-social-login/routes/index.js
+++ node-social-login/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
+import { OAuthStrategy as GoogleStrategy } from 'passport-google-oauth';
+import { Strategy as TwitterStrategy } from 'passport-twitter';
 import environment from '../config/constants';
 const env = environment.ENVIRONMENT;
 process.env.NODE_ENV = env;
@@ -28,9 +30,11 @@ export default passport => {
             done(err, user);
         });
     });
-
+    router.get('/', (req, res) => {
+        res.send('Hello World');
+    });
+    /* FACEBOOK AUTHENTICATION */
     passport.use(new FacebookStrategy(CONSTANTS['SOCIAL']['FACEBOOK'], (accessToken, refreshToken, profile, done) => {
-        // console.log(accessToken, refreshToken, profile)
         User.findOne({ facebookId: profile.id }, (err, user) => {
             if (err) { return done(err) }
             if (user) {
@@ -45,9 +49,6 @@ export default passport => {
     }
     ));
 
-    router.get('/', (req, res) => {
-        res.send('Hello World');
-    });
     router.post('/login', passport.authenticate('login', {
         successRedirect: '/home',
         failureRedirect: '/',
@@ -67,7 +68,61 @@ export default passport => {
             failureRedirect: '/'
         })
     );
+    /* 
+    FACEBOOK AUTHENTICATION DONE
+     */
+    /* GOOGLE AUTHENTICATION START */
 
+    // Use the GoogleStrategy within Passport.
+    //   Strategies in passport require a `verify` function, which accept
+    //   credentials (in this case, a token, tokenSecret, and Google profile), and
+    //   invoke a callback with a user object.
+    passport.use(new GoogleStrategy({
+        consumerKey: "GOOGLE_CONSUMER_KEY",
+        consumerSecret: "GOOGLE_CONSUMER_SECRET",
+        callbackURL: "http://localhost:3000/login/google/callback"
+    }, (token, tokenSecret, profile, done) => {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+    ));
+
+    // GET /auth/google
+    //   Use passport.authenticate() as route middleware to authenticate the
+    //   request.  The first step in Google authentication will involve redirecting
+    //   the user to google.com.  After authorization, Google will redirect the user
+    //   back to this application at /auth/google/callback
+    router.get('/login/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+    // GET /auth/google/callback
+    //   Use passport.authenticate() as route middleware to authenticate the
+    //   request.  If authentication fails, the user will be redirected back to the
+    //   login page.  Otherwise, the primary route function function will be called,
+    //   which, in this example, will redirect the user to the home page.
+    router.get('/login/google/callback',
+        passport.authenticate('google', { failureRedirect: '/' }),
+        (req, res) => {
+            res.redirect('/home');
+        });
+    /* GOOGLE AUTHENTICATION END */
+    /* TWITTER AUTHENTICATION START */
+    passport.use(new TwitterStrategy(CONSTANTS['SOCIAL']['TWITTER'],
+        function (token, tokenSecret, profile, done) {
+            User.findOrCreate({}, function (err, user) {
+                if (err) { return done(err); }
+                done(null, user);
+            });
+        }
+    ));
+
+    router.get('/login/twitter', passport.authenticate('twitter'));
+    router.get('/login/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect: '/home',
+            failureRedirect: '/'
+        }));
+    /* TWITTER AUTHENTICATION END */
     router.get('/home', isAuthenticated, (req, res) => {
         res.send('You are successfully signed up with facebook ');
     });
